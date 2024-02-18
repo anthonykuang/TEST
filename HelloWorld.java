693075efbcdf9a9bad28e8ae379c0ac74a69bddd
anthony <orangecat38@yahoo.com.tw>
2024-02-18 11:13:18 +0800

    6

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("hi");
-		System.out.println("here is 3a");
+		System.out.println("here is 6");
 	}
 }
